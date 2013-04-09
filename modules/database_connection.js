5bc385b137c2713bafdbaf0dd13de47a95266943
Marcos Placona <marcos.placona@gmail.com>
2013-04-09 21:12:09 +0100

    resolving conflicts

--- modules/database_connection.js
+++
@@ -1,8 +0,0 @@
-mongoose	= require('mongoose');
-
-dbPort 		= 27017;
-dbHost 		= 'localhost';
-dbName 		= 'KickAssBio';
-
-db = mongoose.createConnection(dbHost, dbName);
-db.on('error', console.error.bind(console, 'connection error:'));
